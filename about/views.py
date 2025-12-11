82a0c77b42511d701997e120ea7db07cddcfa66f
yinyangsammy <yinyangsammy@gmail.com>
2025-12-11 16:58:14 +0000

    feat: about/views

--- about/views.py
+++ about/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    return HttpResponse("This would be the about page")
